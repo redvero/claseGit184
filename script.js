d7f96248fd0b8a947d8526f552f5963809d6f6eb
redvero <redvero@hotmail.com>
2023-04-17 22:59:41 -0300

    cambie footer, colores de fonde de forms

---
+++ script.js
@@ -0,0 +1,13 @@
+function nosotros () {
+
+    swal("Megathlon es una casa de venta de instrumentos musicales que se dedica a equipar a profesionales, estudiantes y aficionados, en todo el país.\n \n Brindamos asesoramiento a nuestros clientes, tanto en la compra como en el mantenimiento de los instrumentos, porque los que trabajamos en Megathlon somos también músicos en actividad, y sabemos lo que significa tu instrumento para vos.\n\n Visitanos en nuestra página y en nuestros locales. ¡Te esperamos!");  
+}
+function atencion() {
+    swal("Atención al Cliente\nLunes a Viernes de 8:00 a 15:00hs\n0800-9977890");  
+}
+function tecnico() {
+    swal("La Empresa garantiza al comprador de este producto la reparación del mismo y/o reemplazo de los componentes que, a juicio de la empresa resulten con algún defecto de fabricación, dentro del periodo de vigencia de esta garantía que será de 6 (seis) meses, a partir de la fecha de compra. Salvo productos exceptuados ( PARCHES , ARMÓNICAS , DRIVERS , CUERDAS , ACCESORIOS DE PERCUSIÓN ).");
+}
+function privacidad() {
+    swal("Según el Art 22 del Reglamento de la UE 2016/679 se le informa expresamente que podrá ejercitar los derechos de acceso, rectificación, oposición, supresión, limitación, portabilidad y revocación de los consentimientos y autorizaciones concedidas dirigiéndose a nuestra empresa a través de la dirección de correo electrónico info@megathlon.com e indicando textualmente en el asunto: “El ejercicio del derecho solicitado seguido de su nombre completo”.");
+}
